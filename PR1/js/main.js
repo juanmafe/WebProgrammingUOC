bb5b197c55fd43f8bb8aaeda419ffdb04b80ba0b
Juanma <juanreyes.1990@gmail.com>
2024-07-23 23:05:11 +0200

    Delete PR1 directory

--- PR1/js/main.js
+++
@@ -1,3 +0,0 @@
-import Game from './modules/game.js';
-
-new Game();
--- PR1/js/modules/consts/category-list.js
+++
@@ -1,9 +0,0 @@
-const CATEGORY_LIST = [
-    'General Knowledge',
-    'Science & Nature',
-    'Science: Mathematics',
-    'Sports',
-    'Geography',
-];
-
-export { CATEGORY_LIST };
